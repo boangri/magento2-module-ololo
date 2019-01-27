3e38094a6d1130f2be2d795637ccbbfefacb55bf
boris@acer <xinu@yandex.ru>
2019-01-27 18:40:55 +0300

    Added images

--- Block/Main.php
+++ Block/Main.php
@@ -10,48 +10,4 @@ use \Boangri\Ololo\Model\ResourceModel\Portfolio\Collection as PortfolioCollecti
 
 class Main extends Template
 {
-//    /**
-//     * CollectionFactory
-//     * @var null|CollectionFactory
-//     */
-//    protected $_postCollectionFactory = null;
-//
-//    /**
-//     * Constructor
-//     *
-//     * @param Context $context
-//     * @param PostCollectionFactory $postCollectionFactory
-//     * @param array $data
-//     */
-//    public function __construct(
-//        Context $context,
-//        PostCollectionFactory $postCollectionFactory,
-//        array $data = []
-//    ) {
-//        $this->_postCollectionFactory = $postCollectionFactory;
-//        parent::__construct($context, $data);
-//    }
-//
-//    /**
-//     * @return Post[]
-//     */
-//    public function getPosts()
-//    {
-//        /** @var PostCollection $postCollection */
-//        $postCollection = $this->_postCollectionFactory->create();
-//        $postCollection->addFieldToSelect('*')->load();
-//        return $postCollection->getItems();
-//    }
-//
-//    /**
-//     * For a given post, returns its url
-//     * @param Post $post
-//     * @return string
-//     */
-//    public function getPostUrl(
-//        Post $post
-//    ) {
-//        return '/blog/post/view/id/' . $post->getId();
-//    }
-
 }
