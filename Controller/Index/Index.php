76181355e2a923943dc99195998940f6795e031f
boris@acer <xinu@yandex.ru>
2020-02-23 12:03:59 +0300

    Simplified actions

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -2,45 +2,20 @@
 
 namespace Boangri\Ololo\Controller\Index;
 
-use \Magento\Framework\App\Action\Action;
-use \Magento\Framework\View\Result\PageFactory;
-use \Magento\Framework\View\Result\Page;
-use \Magento\Framework\App\Action\Context;
-use \Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\ResultFactory;
 
+/**
+ * Class Index
+ * @package Boangri\Ololo\Controller\Index
+ */
 class Index extends Action
 {
-
     /**
-     * @var PageFactory
-     */
-    protected $resultPageFactory;
-
-    /**
-     * @param Context $context
-     * @param PageFactory $resultPageFactory
-     *
-     * @codeCoverageIgnore
-     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
-     */
-    public function __construct(
-        Context $context,
-        PageFactory $resultPageFactory
-    ) {
-        parent::__construct(
-            $context
-        );
-        $this->resultPageFactory = $resultPageFactory;
-    }
-
-    /**
-     * Prints the blog from informed order id
-     * @return Page
-     * @throws LocalizedException
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface
      */
     public function execute()
     {
-        $resultPage = $this->resultPageFactory->create();
-        return $resultPage;
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
--- Controller/Portfolio/Index.php
+++ Controller/Portfolio/Index.php
@@ -2,45 +2,13 @@
 
 namespace Boangri\Ololo\Controller\Portfolio;
 
-use \Magento\Framework\App\Action\Action;
-use \Magento\Framework\View\Result\PageFactory;
-use \Magento\Framework\View\Result\Page;
-use \Magento\Framework\App\Action\Context;
-use \Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\ResultFactory;
 
 class Index extends Action
 {
-
-    /**
-     * @var PageFactory
-     */
-    protected $resultPageFactory;
-
-    /**
-     * @param Context $context
-     * @param PageFactory $resultPageFactory
-     *
-     * @codeCoverageIgnore
-     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
-     */
-    public function __construct(
-        Context $context,
-        PageFactory $resultPageFactory
-    ) {
-        parent::__construct(
-            $context
-        );
-        $this->resultPageFactory = $resultPageFactory;
-    }
-
-    /**
-     * Prints the blog from informed order id
-     * @return Page
-     * @throws LocalizedException
-     */
     public function execute()
     {
-        $resultPage = $this->resultPageFactory->create();
-        return $resultPage;
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
